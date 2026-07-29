39cbaf898c9fea29c23d15ab502394ee04f7e610
Ferhat Atilla <ferhat.atilla09@gmail.com>
2026-07-29 12:28:37 -0500

    Changes updated

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -1,19 +1,9 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('login test', async ({ page }) => {
+    await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+    await page.locator('#userEmail').fill('ferhatatilla123@example.com');
+    await page.locator('#userPassword').fill('123456');
+    await page.locator('#login').click();
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/dashboard');
 });
